7afcafeae0201b47b262933e66244274cd6db435
iNoob <59546554+iNoobBoi@users.noreply.github.com>
2020-01-08 17:04:25 -0800

    Update Deck.java

--- BlackJack/Deck.java
+++ BlackJack/Deck.java
@@ -2,19 +2,16 @@
 import java.util.Random;
 
 public class Deck {
-	private int newDeck;
 	private int counter = 0;
-	Card cardDeck[] = new Card[this.newDeck]; //creates a deck of 52 cards
+	Card cardDeck[] = new Card[52]; //creates a deck of 52 cards
 	
 	public Deck(int deckCount) {
-		this.newDeck = 52 * deckCount;
-		
 		int deckIndex;
 		
 		for (int i = 0; i < 4; i++) {
 			for (int j = 0; j < 13; j++) {
 				deckIndex = i * j;
-				this.cardDeck[deckIndex - 1].setCard(i, j); //sets the card in 
+				this.cardDeck[deckIndex].setCard(i, j); //sets the card in 
 				} //sets rank of each card to one 13 ranks
 			} //sets suit of each Card to one of 4 suits
 		}
