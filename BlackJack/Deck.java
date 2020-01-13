00c1eca7675f7d7aeb93ac16a2f4b52b03842514
iNoob <59546554+iNoobBoi@users.noreply.github.com>
2020-01-12 18:10:44 -0800

    Update Deck.java

--- BlackJack/Deck.java
+++ BlackJack/Deck.java
@@ -2,33 +2,34 @@
 import java.util.Random;
 
 public class Deck {
-	private int counter = 0;
+	public int counter = 0;
+	public int deckSize = 0;
 	Card cardDeck[] = new Card[52]; //creates a deck of 52 cards
 	
 	public Deck(int deckCount) {
-		int deckIndex;
+		int deckIndex = 0;
 		
 		for (int i = 0; i < deckCount; i++) {
 			for (int j = 0; j < 4; j++) {
 				for (int k = 0; k < 13; k++) {
-					deckIndex = (i * 13) + j;
-					this.cardDeck[deckIndex] = new Card(i, j); //creates a new card based on the values of i and j
+					this.cardDeck[deckIndex] = new Card(j, k); //creates a new card based on the values of i and j
+					deckIndex++;
+					deckSize++;
 					} //sets rank of each card to one 13 ranks
 				} //sets suit of each Card to one of 4 suits
 			} //creates 1 deck of each kind of card and then repeats until all of the decks are prepared
 		}
 	
 	public void shuffleDeck() {
-		int index;
 		Random random = new Random();
-		for (int i = 0; i < this.counter; i++) {
-			index = random.nextInt(i + 1);
-			if (index != i) {
-				this.cardDeck[index] = this.cardDeck[i];
-				this.cardDeck[i] = this.cardDeck[index];
-				this.cardDeck[index] = this.cardDeck[i];
-				}
-			}
+		Card temp;
+		for(int i = 0; i < 200; i++) {
+			int posOne = random.nextInt(deckSize - 1);
+			int posTwo = random.nextInt(deckSize - 1);
+			temp = cardDeck[posTwo];
+			cardDeck[posTwo] = cardDeck[posOne];
+			cardDeck[posOne] = temp;
+			} //swaps 200 random card indexes in the deck
 		} //shuffles the deck
 	
 	public Card drawCard() {
