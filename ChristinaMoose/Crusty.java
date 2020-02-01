267445b426f03b9b9bc373d46132447bcb2d54ed
iNoob <59546554+iNoobBoi@users.noreply.github.com>
2020-02-01 11:55:34 -0800

    Create Crusty.java

---
+++ ChristinaMoose/Crusty.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Crusty {
+  public static void main(String []args) {
+    Scanner input;
+    input = new Scanner(System.in);
+    
+    String Crusty = input.nextLine();
+    System.out.println("Crusty " + Crusty);
+    }
+  }
