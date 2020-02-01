c8f5494442a8fbb0bdf78be4748bf214da868794
iNoob <59546554+iNoobBoi@users.noreply.github.com>
2020-02-01 12:08:53 -0800

    Update CrustyArray.java

--- ChristinaMoose/JavaTest/CrustyArray.java
+++ ChristinaMoose/JavaTest/CrustyArray.java
@@ -1,13 +1,14 @@
 public class CrustyArray {
-  public static void main(String []args) {
+ public static void main(String []args) {
     int [] intArray = {4, 3, 2, 1, 0};
     int intArrayThing [] = new int[3];
     
+    System.out.println("\nBefore Change");
     System.out.println(intArray[0]);
     System.out.println(intArray[1]);
-    System.out.print(intArray[2]);
+    System.out.println(intArray[2]);
     System.out.println(intArray[3]);
-    System.out.print(intArray[4]);
+    System.out.println(intArray[4]);
     
     intArray[0] = 0;
     intArray[1] = 1;
@@ -15,10 +16,11 @@ public class CrustyArray {
     intArray[3] = 3;
     intArray[4] = 4;
     
+    System.out.println("\nAfter Change");
     System.out.println(intArray[0]);
     System.out.println(intArray[1]);
-    System.out.print(intArray[2]);
+    System.out.println(intArray[2]);
     System.out.println(intArray[3]);
-    System.out.print(intArray[4]);
+    System.out.println(intArray[4]);
     }
   }
