bf0c48242bccef930be2b9ada36eb9b881b53c69
iNoob <59546554+iNoobBoi@users.noreply.github.com>
2020-02-01 12:15:42 -0800

    Update ranDom.java

--- iNoob/Random/ranDom.java
+++ iNoob/Random/ranDom.java
@@ -1,13 +1,13 @@
 public class ranDom {
   public static void main(String []args) {
     for (int i = 0; i < 10; i++) {
-      for (int j = 0; j < 10; j++) {
-        for (int k = 0; k < 10; k++) {
+      for (int j = 0; j < i; j++) {
+        for (int k = 0; k < j; k++) {
           System.out.print("*");
           }
-         System.out.print("*");
+         System.out.println("*");
         }
-       System.out.print("*");
+       System.out.println("\n*");
       }
     }
   }
