8ff47cd43dc3472bfd7a39f0877da0ad4bf974c5
iNoob <59546554+iNoobBoi@users.noreply.github.com>
2020-02-01 12:02:01 -0800

    Create CrustyArray.java

---
+++ ChristinaMoose/JavaTest/CrustyArray.java
@@ -0,0 +1,11 @@
+public class CrustyArray {
+  public static void main(String []args) {
+    int [] intArray = new int[5];
+    int intArrayThing [] = new int[3];
+    int [] intArrayy = {0, 1, 03894};
+    
+    for (int i = 0; i < intArrayy.length(); i++) {
+      System.out.println(intArrayy[i]);
+      }
+    }
+  }
