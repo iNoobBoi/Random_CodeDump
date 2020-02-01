5026738a31401461f96b9d7eaa96902a158b7ad7
iNoob <59546554+iNoobBoi@users.noreply.github.com>
2020-02-01 12:07:10 -0800

    Update CrustyArray.java

--- ChristinaMoose/JavaTest/CrustyArray.java
+++ ChristinaMoose/JavaTest/CrustyArray.java
@@ -1,11 +1,24 @@
 public class CrustyArray {
   public static void main(String []args) {
-    int [] intArray = new int[5];
+    int [] intArray = {4, 3, 2, 1, 0};
     int intArrayThing [] = new int[3];
-    int [] intArrayy = {0, 1, 03894};
     
-    for (int i = 0; i < intArrayy.length(); i++) {
-      System.out.println(intArrayy[i]);
-      }
+    System.out.println(intArray[0]);
+    System.out.println(intArray[1]);
+    System.out.print(intArray[2]);
+    System.out.println(intArray[3]);
+    System.out.print(intArray[4]);
+    
+    intArray[0] = 0;
+    intArray[1] = 1;
+    intArray[2] = 2;
+    intArray[3] = 3;
+    intArray[4] = 4;
+    
+    System.out.println(intArray[0]);
+    System.out.println(intArray[1]);
+    System.out.print(intArray[2]);
+    System.out.println(intArray[3]);
+    System.out.print(intArray[4]);
     }
   }
