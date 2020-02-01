d3039d416950d8a4b17d8ac474d7e65aef1fa7f3
iNoob <59546554+iNoobBoi@users.noreply.github.com>
2020-02-01 12:12:22 -0800

    Create ranDom.java

---
+++ iNoob/Random/ranDom.java
@@ -0,0 +1,13 @@
+public class ranDom {
+  public static void main(String []args) {
+    for (int i = 0; i < 10; i++) {
+      for (int j = 0; j < 10; j++) {
+        for (int k = 0; k < 10; k++) {
+          System.out.print("*");
+          }
+         System.out.print("*");
+        }
+       System.out.print("*");
+      }
+    }
+  }
